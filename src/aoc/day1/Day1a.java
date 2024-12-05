0868ff01b97e17aba00f9158fdb7b40ae47eb1d1
AmigosKazz <kaznarahandrinarivo@gmail.com>
2024-12-05 09:09:50 +0300

    config Util

---
+++ src/aoc/day1/Day1a.java
@@ -0,0 +1,12 @@
+package aoc.day1;
+
+import aoc.Util;
+
+import java.io.IOException;
+import java.util.List;
+
+public class Day1a {
+    public static void main(String[] args) throws IOException {
+        List<String> lines = Util.readInput(false, 1, false);
+    }
+}
